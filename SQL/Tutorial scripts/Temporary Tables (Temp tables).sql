a04a178a48f96afaed6370b8ff50c1861e022771
Maxime Vilcocq <a01710550@tec.mx>
2026-08-24 18:21:20 -0600

    Temporary tables notes

---
+++ SQL/Tutorial scripts/Temporary Tables (Temp tables).sql
@@ -0,0 +1,41 @@
+# Temporary tables
+
+# Son similares a las CTE
+# Sirven para almacenar temporalmente resultados de alguna consulta que se vaya a reutilizar
+# Se eliminan al volver a acceder a SQL, solo duran lo que dure la sesión en la que fueron creados
+
+#create temporary table
+
+create temporary table temp_table
+(
+first_name varchar(50),
+last_name varchar(50),
+favorite_movie varchar(50)
+);
+
+select *
+from temp_table;
+
+-- Insertar valores:
+
+insert into temp_table
+values("Maxime", "Last Name", "1984");
+
+select *
+from temp_table;
+
+# Eliminar la tabla
+#drop table temp_table;
+
+
+-- Forma alternativa
+
+select *
+from parks_and_recreation.employee_salary;
+
+create temporary table salary_over_50k
+select * from parks_and_recreation.employee_salary
+where salary > 50000;
+
+select *
+from salary_over_50k;
