0f826caff84b26cb4164200dc3cbac193cac59d4
Maxime Vilcocq <a01710550@tec.mx>
2026-09-11 18:12:13 -0600

    Modules

---
+++ Python/Jupyter/Modules/modulesMax.py
@@ -0,0 +1,23 @@
+"""
+Este módulo sirve como ejemplo para explicar modulos
+
+Incluye una función documentada de suma y una función no documentada de resta
+"""
+
+def suma(a,b):
+  """
+  Calcula el total de sumar 2 números
+  
+  Args:
+    a (float): el primer número
+    b (float): el segundo número
+
+  Returns:
+    float: la suma de los dos números
+  """
+  return a + b
+
+def resta(a,b):
+  return a - b
+
+lista = ["a","b","c","d"]
